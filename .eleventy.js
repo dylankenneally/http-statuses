d91d613b85ff449a35e4bc9ec6d21b9e9902f3cb
Dylan Kenneally <dylan.kenneally@gmail.com>
2023-01-05 21:56:47 +1100

    Basic 404 stub

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 require('dotenv').config();
 
 const dir = {
@@ -5,9 +6,34 @@ const dir = {
 	output: 'build'
 };
 
+// Check there 404 input
+const _404input = `${dir.input}/404.md`;
+if (!fs.existsSync(_404input)) {
+	throw new Error(`Failed to find the input for the 404 page, expected it to be at ${_404input}`);
+}
+
+const _404page = `${dir.output}/404.html`;
+
 module.exports = (eleventyConfig) => {
-	// auto open the browser when we use `npx eleventy --serve` (i.e. when we use `npm start`)
-	eleventyConfig.setBrowserSyncConfig({ open: true });
+	eleventyConfig.setBrowserSyncConfig({
+		// auto open the browser when we use `npx eleventy --serve` (i.e. when we use `npm start`)
+		open: true,
+
+		// Add 404 support to browser sync
+		callbacks: {
+			ready: (err, bs) => {
+				bs.addMiddleware('*', (req, res) => {
+					if (!fs.existsSync(_404page)) {
+						throw new Error(`Failed to find the 404 page, expected it to be at ${_404page}`);
+					}
+
+					res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' });
+					res.write(fs.readFileSync(_404page));
+					res.end();
+				});
+			}
+		}
+	});
 
 	const cssDir = `${dir.input}/css/`;
 	eleventyConfig.addPassthroughCopy(cssDir);
@@ -16,6 +42,6 @@ module.exports = (eleventyConfig) => {
 	return {
 		dir,
 		markdownTemplateEngine: 'njk',
-		pathPrefix: process.env.DEPLOY_PATH,
+		pathPrefix: process.env.DEPLOY_PATH
 	};
 };
