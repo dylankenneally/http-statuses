936c34f8caa07e41f544ce3b7bed03404eaddbea
Dylan Kenneally <dylan.kenneally@gmail.com>
2023-03-04 10:17:36 +1100

    printf debuggging

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -4,8 +4,12 @@ const sitemapUrl = process.env.SITEMAP_URL;
 
 // const axios = require("axios");
 
+console.log('*=*=*=*=* dep suc script');
+
 exports.handler = async (event) => {
+  console.log('*=*=*=*=* dep suc event');
   if (!sitemapUrl) {
+    console.log('*=*=*=*=* no url');
     return {
       statusCode: 400, /* bad request */
       body: 'The sitemap URL is missing'
@@ -13,10 +17,12 @@ exports.handler = async (event) => {
   }
 
   try {
+    console.log('*=*=*=*=* paylod');
     const { payload } = JSON.parse(event.body);
     const { state, context } = payload;
 
     if (state !== stateCondition || context !== contextCondition) {
+      console.log(`*=*=*=*=* no con { state: ${stateCondition}, context: ${contextCondition} }, current: { state: ${state}, context: ${context} }`);
       return {
         statusCode: 502, /* bad gateway */
         body: `State and/or context mismatched. Required: { state: ${stateCondition}, context: ${contextCondition} }, current: { state: ${state}, context: ${context} }.`
@@ -27,6 +33,7 @@ exports.handler = async (event) => {
     // await axios.get(`http://www.google.com/ping?sitemap=${sitemapUrl}`);
     return { statusCode: 200 };
   } catch (err) {
+    console.log('*=*=*=*=* fucked');
     console.error(err);
     throw err;
   }
